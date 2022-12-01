40c7b76c24ade0866c7aa0347b0ad25b3362a8aa
wcz0 <wcz0@outlook.com>
2022-12-01 10:17:34 +0800

    init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Godruoyi\Snowflake\LaravelSequenceResolver;
+use Godruoyi\Snowflake\Snowflake;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->singleton('snowflake', function($app){
+            return (new Snowflake)
+                ->setStartTimeStamp(strtotime(config('snowflake.start_time'))*1000)
+                ->setSequenceResolver(new LaravelSequenceResolver($app->get('cache.store')));
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ config/snowflake.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'start_time' => env('SNOWFLAKE_START', '2022-01-01'),
+];
